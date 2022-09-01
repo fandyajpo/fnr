500e4e544591543d6282d8af0debaac46b9accde
Fandoy <fandyglitch3@gmail.com>
2022-09-01 23:09:38 +0800

    init cmt

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "lib/context";
+import { orderPrice } from "lib/redux";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  store.dispatch(orderPrice());
+  const Layout = Component.layout || (({ children }) => <>{children}</>);
+
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
